ee7deda2af3998bf7e9f42ab06dfc646973bb7c6
Bax.dev <basseyeyo991@gmail.com>
2025-04-19 17:17:51 +0100

    db data

--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -1,6 +1,6 @@
 
-const API_BASE_URL = "http://localhost:5000/api/";
-// const API_BASE_URL = "https://pos-backend-production-9b1f.up.railway.app/api/";
+// const API_BASE_URL = "http://localhost:5000/api/";
+const API_BASE_URL = "https://pos-backend-production-9b1f.up.railway.app/api/";
 
 export const API_ROUTES = {
   REGISTER: `${API_BASE_URL}auth/register`,
